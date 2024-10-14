a0840e25a1ed5105d626a7e9376e559c31919e67
Gyanvendra Singh <gyanvendras2004@gmail.com>
2024-10-14 22:19:56 +0530

    first commit

---
+++ variable.js
@@ -0,0 +1,14 @@
+// let gretings: string ="hello gyani"
+// console.log(gretings);
+// export {}
+//  FUCNTION IN TYPESCRIPT 
+// function greet(FirtName:String) {
+//     console.log("Hello "+FirtName);
+// }
+// greet("GYANVENDRA")
+// export{}
+// SUM FUNCTION
+function sum(num1, num2) {
+    console.log("THE SUM OF THE TWO NUMBERS ARE: " + (num1 + num2));
+}
+sum(20, 10);
